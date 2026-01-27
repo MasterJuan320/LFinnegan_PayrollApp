1778cca3123aa247da55e1ab6db4e0b7575fc029
Liam Finnegan <liamfinnegan0@gmail.com>
2026-01-27 10:43:29 +0000

    Second Version

--- payroll.py
+++ payroll.py
@@ -15,13 +15,29 @@ INPUT_RATE_CSV_FILE = os.path.join(CURRENT_DIRECTORY, 'Input', 'rates.csv')
 INPUT_TIMETABLE_CSV_FILE = os.path.join(CURRENT_DIRECTORY, 'Input', 'hours.csv')
 
 
+def read_csv(filename, dict_update, field_name):
+    """
+    Docstring for read_csv
+    
+    :param filename: Description
+    :param dict_update: Description
+    :param field_name: Description
+    """
+    with open(filename, encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            ppsn = row['PPSN']
+            field = row[field_name]
+            person_dict = dict_update.get(row['PPSN'], {})
+            person_dict[field_name] = field
+            dict_update[ppsn] = person_dict
+
 salary_dict = {}
 
-with open(INPUT_PPSN_CSV_FILE, encoding='utf-8') as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        ppsn = row['PPSN']
-        name = row['Name']
-        person_dict = salary_dict.get(row['PPSN'], {})
-        person_dict['name'] = name
-        salary_dict[ppsn] = person_dict
+read_csv(INPUT_PPSN_CSV_FILE, salary_dict, 'Name')
+read_csv(INPUT_BENIFITS_CSV_FILE, salary_dict, 'Benefits')
+read_csv(INPUT_BONUSES_CSV_FILE, salary_dict, 'Bonuses')
+read_csv(INPUT_RATE_CSV_FILE, salary_dict, 'Rate')
+read_csv(INPUT_TIMETABLE_CSV_FILE, salary_dict, 'Hours')
+
+print(salary_dict)

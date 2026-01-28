a3d2fd881adb8c06036fc34cc17f8b5b1a761dc9
Liam Finnegan <liamfinnegan0@gmail.com>
2026-01-28 14:14:04 +0000

    Implement tax calculation logic in calc_taxes function

--- payroll.py
+++ payroll.py
@@ -6,6 +6,7 @@
 
 import os
 import csv
+import pprint
 
 CURRENT_DIRECTORY = os.path.dirname((__file__))
 
@@ -31,6 +32,27 @@ def read_csv(filename, dict_update, field_name):
             person_dict[field_name] = field
             dict_update[ppsn] = person_dict
 
+def calc_taxes(record_to_update):
+    """Calculate the taxes for a given record.
+    The taxes are calculated based on the salary + bonuses. If the total is less than 3000,
+    the tax rate is 20%. If the total is 3000 or more,
+    the tax rate is 40% over 3000.
+    Parameters
+    ----------
+    record_to_update : dict
+    The record to update with the calculated taxes.
+    Returns
+    -------
+    None
+    """
+    salary_bonuses = record_to_update.get('Salary', 0) + record_to_update.get('Bonuses', 0)
+    if salary_bonuses < 3000:
+        record_to_update['Taxes'] = round(salary_bonuses * 0.2, 2)
+    else:
+        record_to_update['Taxes'] = round((salary_bonuses - 3000) * 0.4 + 3000 * 0.2, 2)
+
+
+
 salary_dict = {}
 
 read_csv(INPUT_PPSN_CSV_FILE, salary_dict, 'Name')
@@ -39,4 +61,10 @@ read_csv(INPUT_BONUSES_CSV_FILE, salary_dict, 'Bonuses')
 read_csv(INPUT_RATE_CSV_FILE, salary_dict, 'Rate')
 read_csv(INPUT_TIMETABLE_CSV_FILE, salary_dict, 'Hours')
 
-print(salary_dict)
+for ppsn, record in salary_dict.items():
+    record['Salary'] = round(record.get('Hours', 0) * record.get('Rate', 0), 2)
+
+for ppsn, record in salary_dict.items():
+    calc_taxes(record)
+
+pprint.pprint(salary_dict)
